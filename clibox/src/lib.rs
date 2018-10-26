caef25f79b562360da06398abce241c0b1025ca1
Frederik De Bleser <frederik@debleser.be>
2018-10-26 16:33:16 +0200

    Working version with argument lookup.
    This is not ideal, since we have to find the name of the port in the
    vector every time. However, this works around the lifetime issues.

--- clibox/src/lib.rs
+++ clibox/src/lib.rs
@@ -5,54 +5,67 @@ mod node;
 pub use self::port::{PortPolarity, PortKind, PortValue, Port};
 pub use self::node::{Node, NodeData};
 
-struct NullNode<'a> {
-    data: NodeData<'a>,
+struct NullNode {
+    data: NodeData
 }
 
-impl<'a> NullNode<'a> {
-    fn new() -> NullNode<'a> {
+impl NullNode {
+    fn new() -> NullNode {
         NullNode {
             data: NodeData::new("Null", 0, 0)
         }
     }
 }
 
-impl<'a> Node<'a> for NullNode<'a> {
-    fn get_node_data(&'a self) -> &'a NodeData { &self.data }
-    fn get_node_data_mut(&'a mut self) -> &'a mut NodeData { &mut self.data }
+impl Node for NullNode {
+    fn get_node_data(&self) -> & NodeData { &self.data }
+    fn get_node_data_mut(&mut self) -> &mut NodeData { &mut self.data }
 
     fn run(&mut self) {}
 }
 
-struct AddNode<'a> {
-    data: NodeData<'a>,
-    in_a: Port,
-    in_b: Port,
-    out: Port,
+struct AddNode {
+    data: NodeData,
+    //in_a: &'a Port,
+    //in_b: &'a Port,
+    //out: &'a Port,
 }
 
-impl<'a> AddNode<'a> {
-    fn new() -> AddNode<'a> {
-        let in_a = Port::new_input("a", PortKind::Float);
-        let in_b = Port::new_input("b", PortKind::Float);
-        let out = Port::new_output("out", PortKind::Float);
-        let data = NodeData::new("Add", 0, 0);
-        let mut node = AddNode { data, in_a, in_b, out };
+impl AddNode {
+    pub fn new() -> AddNode {
+        // let in_a = Port::new_input("a", PortKind::Float);
+        // let in_b = Port::new_input("b", PortKind::Float);
+        // let out = Port::new_output("out", PortKind::Float);
+        let mut data = NodeData::new("Add", 0, 0);
+        data.inputs.push(Port::new_input("a", PortKind::Float));
+        data.inputs.push(Port::new_input("b", PortKind::Float));
+        data.outputs.push(Port::new_output("out", PortKind::Float));
+
+        AddNode { data }
         // node.get_node_data_mut().inputs.push(&node.in_a);
         // node.get_node_data_mut().inputs.push(&node.in_b);
         // node.get_node_data_mut().outputs.push(&node.out);
-        node
+        //node
     }
 }
 
-impl<'a> Node<'a> for AddNode<'a> {
-    fn get_node_data(&'a self) -> &'a NodeData { &self.data }
-    fn get_node_data_mut(&'a mut self) -> &'a mut NodeData { &mut self.data }
+impl Node for AddNode {
+    fn get_node_data(&self) -> &NodeData { &self.data }
+    fn get_node_data_mut(&mut self) -> &mut NodeData { &mut self.data }
 
     fn run(&mut self) {
-        let a = self.in_a.to_float();
-        let b = self.in_b.to_float();
-        self.out.set_float(a + b);
+        let a = self.get_input("a").unwrap().to_float();
+        let b = self.get_input("b").unwrap().to_float();
+        self.get_output_mut("out").unwrap().set_float(a + b);
+        //self.out.set_float(a + b);
+    }
+}
+
+pub fn new_node(type_name: &str) -> Option<Box<Node>> {
+    match type_name {
+        "null" => Some(Box::new(NullNode::new())),
+        "add" => Some(Box::new(AddNode::new())),
+        _ => None
     }
 }
 
@@ -62,12 +75,16 @@ mod test {
 
     #[test]
     fn create_node() {
-        let node = AddNode::new();
+        let mut node = new_node("add").unwrap();
         assert_eq!(node.get_name(), "Add");
         assert_eq!(node.get_inputs().len(), 2);
         assert_eq!(node.get_outputs().len(), 1);
         assert_eq!(node.get_inputs()[0].name, "a");
         assert_eq!(node.get_inputs()[1].name, "b");
         assert_eq!(node.get_outputs()[0].name, "out");
+        node.set_float("a", 3.0);
+        node.set_float("b", 5.0);
+        node.run();
+        assert_eq!(node.get_float_output("out").unwrap(), 8.0);
     }
 }
--- clibox/src/node.rs
+++ clibox/src/node.rs
@@ -1,14 +1,14 @@
 use super::{Port};
 
-pub struct NodeData<'a> {
+pub struct NodeData {
     pub name: String,
     pub x: i32,
     pub y: i32,
-    pub inputs: Vec<&'a Port>,
-    pub outputs: Vec<&'a Port>,
+    pub inputs: Vec<Port>,
+    pub outputs: Vec<Port>,
 }
 
-impl<'a> NodeData<'a> {
+impl NodeData {
     pub fn new(name: &str, x: i32, y: i32) -> NodeData {
         NodeData {
             name: name.to_owned(),
@@ -20,16 +20,32 @@ impl<'a> NodeData<'a> {
     }
 }
 
-pub trait Node<'a> {
-    fn get_node_data(&'a self) -> &'a NodeData;
-    fn get_node_data_mut(&'a mut self) -> &'a mut NodeData;
+pub trait Node {
+    fn get_node_data(& self) -> & NodeData;
+    fn get_node_data_mut(& mut self) -> & mut NodeData;
     fn run(&mut self);
 
-    fn get_name(&'a self) -> String { self.get_node_data().name.clone() }
-    fn get_x(&'a self) -> i32 { self.get_node_data().x }
-    fn get_y(&'a self) -> i32 { self.get_node_data().y }
-    fn get_inputs(&'a self) -> &Vec<&Port> { &self.get_node_data().inputs }
-    fn get_outputs(&'a self) -> &Vec<&Port> { &self.get_node_data().outputs }
+    fn get_name(& self) -> String { self.get_node_data().name.clone() }
+    fn get_x(& self) -> i32 { self.get_node_data().x }
+    fn get_y(& self) -> i32 { self.get_node_data().y }
+    fn get_inputs(& self) -> &Vec<Port> { &self.get_node_data().inputs }
+    fn get_outputs(& self) -> &Vec<Port> { &self.get_node_data().outputs }
+    fn get_input(&self, name: &str) -> Option<&Port> { self.get_node_data().inputs.iter().find(|p| p.name == name) }
+    fn get_input_mut(&mut self, name: &str) -> Option<&mut Port> { self.get_node_data_mut().inputs.iter_mut().find(|p| p.name == name) }
+    fn get_output(&self, name: &str) -> Option<&Port> { self.get_node_data().outputs.iter().find(|p| p.name == name) }
+    fn get_output_mut(&mut self, name: &str) -> Option<&mut Port> { self.get_node_data_mut().outputs.iter_mut().find(|p| p.name == name) }
+    fn set_float(&mut self, name: &str, v: f32) { 
+        match self.get_input_mut(name) {
+            None => {},
+            Some(input) => input.set_float(v)
+        }
+    }
+    fn get_float_output(&self, name: &str) -> Option<f32> { 
+        match self.get_output(name) {
+            None => None,
+            Some(port) => Some(port.to_float())
+        }
+    }
 }
 
 // pub struct Node {
